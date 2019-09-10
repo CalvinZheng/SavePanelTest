93c91ac5387d9a5542e4d7857c79bd4948a93bd6
Calvin Zheng <calvin.zheng@corel.com>
2019-09-10 15:57:03 -0400

    test code

--- SavePanelTest/AppDelegate.m
+++ SavePanelTest/AppDelegate.m
@@ -11,17 +11,34 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (strong) NSSavePanel* uiPanel;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	// Insert code here to initialize your application
+	
+	self.uiPanel = [[NSSavePanel alloc] init];
+	NSView* accView = [[NSView alloc] initWithFrame:NSMakeRect(0, 0, 100, 100)];
+	NSButton* testButton = [NSButton checkboxWithTitle:@"Test" target:self action:@selector(testAction)];
+	[accView addSubview:testButton];
+	self.uiPanel.accessoryView = accView;
+	
+	if ( ([self.uiPanel runModal] == NSModalResponseOK ) )
+	{
+		;
+	}
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
 	// Insert code here to tear down your application
+}
+
+- (void)testAction
+{
+	self.uiPanel.nameFieldStringValue = @"xxxx";
 }
 
 
